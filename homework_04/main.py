9d171affe598e75a0568384e2f2cd8f5f12fb049
Anatolii <anatol1812@gmail.com>
2023-06-07 22:55:46 +0100

    Finished all task

--- homework_04/main.py
+++ homework_04/main.py
@@ -12,14 +12,67 @@
   (используйте полученные из запроса данные, передайте их в функцию для добавления в БД)
 - закрытие соединения с БД
 """
+import asyncio
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+)
+
+from jsonplaceholder_requests import get_json_data
+from models import (
+    Base,
+    User,
+    Post,
+    engine,
+    Session, )
+
+
+async def create_all_users(async_session: AsyncSession, users: list):
+    async_session.add_all(users)
+    await async_session.commit()
+
+
+async def create_all_posts(async_session: AsyncSession, posts: list):
+    async_session.add_all(posts)
+    await async_session.commit()
 
 
 async def async_main():
-    pass
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    user_data, post_data = await asyncio.gather(
+        get_json_data("users"),
+        get_json_data("posts")
+    )
+
+    # Create SQLAlchemy instances from each JSON object
+    users = []
+    posts = []
+    for user in user_data:
+        new_user = User(
+            name=user['name'],
+            username=user['username'],
+            email=user["email"],
+        )
+        users.append(new_user)
+
+    for post in post_data:
+        new_post = Post(
+            title=post['title'],
+            body=post['body'],
+            user_id=post["userId"],
+        )
+        posts.append(new_post)
+
+    async with Session() as session:
+        await create_all_users(session, users)
+        await create_all_posts(session, posts)
 
 
 def main():
-    pass
+    asyncio.run(async_main())
 
 
 if __name__ == "__main__":
