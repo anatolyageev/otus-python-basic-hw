956e1c038ef3bdfacbd14a3bda852e51339068fc
Anatolii <anatol1812@gmail.com>
2023-06-25 17:11:44 +0100

    compleated homework_05

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,12 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+from flask import Flask
+
+from views.index import index_app
+
+app = Flask(__name__)
+app.register_blueprint(index_app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
